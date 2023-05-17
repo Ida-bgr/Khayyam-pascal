7a1b7ff5c47ba17d77f2dfc859b182362c2b7021
Ida-bgr <aida.barzegars@gmail.com>
2023-05-17 20:34:50 +0430

    khayyam pascal debug

---
+++ eatingAnElephent.js
@@ -0,0 +1,28 @@
+// ? im goint to eat an elephent
+// ? here is how to eat an elephent => one peace at a time
+
+// * first we mesure what pieces of the element should we eat first
+
+
+const elephentPices = [
+    'head',
+    'sholder',
+    'foot',
+    'etc'
+]
+
+// * then we plan based on the result of the step one
+
+
+for (let i = 0; i < elephentPices.length; i++) {
+    const piceOfTheElephent = elephentPices[i];
+
+    eatElephent()
+    
+}
+
+// * then we start eating pice number 1
+function eatElephent(){
+
+}
+// * ....
